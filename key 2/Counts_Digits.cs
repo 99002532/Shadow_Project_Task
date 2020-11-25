ca48fdad0b80fd4363c3df8c2ada6cc804e8742c
Gaurav <gauravdaslaban@gmail.com>
2020-11-25 10:39:41 +0530

    Counting Digits

---
+++ key 2/Counts_Digits.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+
+
+namespace KeyGen
+{
+    interface ICounts_Digits
+    {
+        public void Digi_Counter(ArrayList digis);
+    }
+    class Counts_Digits : ICounts_Digits
+    {
+        /// <summary>
+        /// DigitConter:
+        /// this function will count the digits.if it is more than 3 digits program will show an error message and exit.
+        /// </summary>
+        /// <param name="digis"></param>
+        public void Digi_Counter(ArrayList digis)
+        {
+            if (digis.Count > 3)
+            {
+                Console.WriteLine("Maximum three digits are accepted");
+                Console.ReadLine();
+                System.Environment.Exit(0);
+            }
+        }
+        public void Digi_special(ArrayList digits)
+        {
+            String digi_str;
+            try
+            {
+                digi_str = digits[0].ToString();
+            }
+            catch(Exception e)
+            {
+                return;
+            }
+            digi_str = digits[0].ToString();
+            char[] digi_char = digi_str.ToCharArray();
+
+
+            for (int i = 0; i < digi_char.Length; i++)
+            {
+                digi_str = digits[i].ToString();
+                digi_char = digi_str.ToCharArray();
+                //Console.WriteLine("Characters: " + digi_char[i]);
+                if (Char.IsLetter(digi_char[i]))
+                {
+                    Console.WriteLine("No Characters allowed in input");
+                    Console.ReadLine();
+                    System.Environment.Exit(0);
+                }
+
+            }
+
+        }
+    }
+}
