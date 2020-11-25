d8a414e2d2da1d7563776327680dfeb60c80d901
Gaurav <gauravdaslaban@gmail.com>
2020-11-25 10:41:20 +0530

    Update Program.cs

--- key 2/Program.cs
+++ key 2/Program.cs
@@ -1,1 +1,18 @@
+using System;
+using System.Collections;
+using System.IO;
+using System.Text.Json;
+using System.Linq;
+using KeyGen;
 
+namespace MyApplication
+{
+
+    public static class Globals
+    {
+
+        public static int FlagG = 0;
+        public static int FlagOne = 0;
+        public static int FlagTwo = 0;
+    }
+    
