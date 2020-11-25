40aa379a91bc4ea1bc7064c03924619c031f5af7
99002511 <72496803+99002511@users.noreply.github.com>
2020-11-25 10:34:00 +0530

    Random Key Generation

---
+++ key 2/KeyGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeyGen
+{
+    interface IKeyGenerator
+    {
+        public int RandomKey(int min, int max);
+    }
+    class KeyGenerator:IKeyGenerator
+{
+    /// <summary>
+    /// Random Key Generator
+    /// it gets min and max value as parameter and return a Random number
+    /// </summary>
+    /// <param name="min"></param>
+    /// <param name="max"></param>
+    /// <returns></returns>
+
+    public int RandomKey(int min, int max)
+    {
+        Random ran = new Random();
+        int a = ran.Next(min, max);
+        return a;
+    }
+};
+}
