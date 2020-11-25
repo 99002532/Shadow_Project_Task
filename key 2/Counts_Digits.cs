261056055e6b0b75fef7ff4bc11d842cb0bc1ec0
99002532 <72495925+99002532@users.noreply.github.com>
2020-11-25 15:13:22 +0530

    Update Counts_Digits.cs

--- key 2/Counts_Digits.cs
+++ key 2/Counts_Digits.cs
@@ -24,6 +24,21 @@ namespace KeyGen
                 System.Environment.Exit(0);
             }
         }
+        public void DigiOneDigit(ArrayList DigisCheck)
+        {
+            foreach (var ValuePrinter in DigisCheck)
+            {
+                string ValueStr = ValuePrinter.ToString();
+                int ValueInt = (int)ValueStr.Count();
+
+                if (ValueInt>1)
+                {
+                    Console.WriteLine("Double Digits not allowed");
+                    Console.ReadLine();
+                    System.Environment.Exit(0);
+                }
+             }
+        }
         public void Digi_special(ArrayList digits)
         {
             String digi_str;
@@ -35,16 +50,16 @@ namespace KeyGen
             {
                 return;
             }
-            digi_str = digits[0].ToString();
+           digi_str = digits.ToString();
             char[] digi_char = digi_str.ToCharArray();
 
 
-            for (int i = 0; i < digi_char.Length; i++)
+            for (int i = 0; i < digits.Count; i++)
             {
                 digi_str = digits[i].ToString();
                 digi_char = digi_str.ToCharArray();
                 //Console.WriteLine("Characters: " + digi_char[i]);
-                if (Char.IsLetter(digi_char[i]))
+                if (Char.IsLetter(digi_char[0]))
                 {
                     Console.WriteLine("No Characters allowed in input");
                     Console.ReadLine();
