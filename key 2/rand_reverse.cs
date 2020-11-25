2271f77dd9ad33fa87c2b6c3ac2ccefb02b09c9d
Gaurav <gauravdaslaban@gmail.com>
2020-11-25 10:40:37 +0530

    Random to arraylist

---
+++ key 2/rand_reverse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace KeyGen
+{
+    interface Irand_reverse
+    {
+        public ArrayList reverse_digiter(int inputnum);
+    }
+    class rand_reverse : Irand_reverse
+{
+    /// <summary>
+    /// This function will parse the interger data into array list. the data will be in reverse manner. 
+    /// </summary>
+    /// <param name="inputnum"></param>
+    /// <returns></returns>
+    public ArrayList reverse_digiter(int inputnum)
+    {
+        int i = 0, m;
+        ArrayList reverse_array = new ArrayList();
+        while (inputnum > 0)
+        {
+            m = inputnum % 10;
+            reverse_array.Add(m);
+            inputnum = inputnum / 10;
+            i++;
+        }
+        return reverse_array;
+    }
+};
+}
